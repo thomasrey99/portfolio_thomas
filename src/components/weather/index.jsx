c5b1f122272bd07df12f855999caa8f69ab342be
Thomasrey99 <thomas_rey1999@outlook.com>
2026-01-08 13:38:13 -0300

    delete weather

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,55 +1,38 @@
 "use client";
-import { getWeatherByCoordinates } from "@/libs/weather";
+
 import { useEffect, useState } from "react";
 
 const Weather = () => {
+
   const [weather, setWeather] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async (position) => {
-          const { latitude, longitude } = position.coords;
-          try {
-            const weatherData = await getWeatherByCoordinates(
-              latitude,
-              longitude
-            );
-            setWeather(weatherData);
-            setLoading(false);
-          } catch (err) {
-            setError("No se pudo obtener el clima");
-            setLoading(false);
-          }
-        },
-        (err) => {
-          setError("No se pudo obtener la ubicación");
-          setLoading(false);
-        }
-      );
-    } else {
-      setError("Geolocalización no soportada");
-      setLoading(false);
-    }
+    navigator.geolocation.getCurrentPosition(pos=>{
+      fetchWeather(pos.coords.latitude, pos.coords.longitude);
+    });
   }, []);
 
+  const fetchWeather = async (lat, lon) => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`
+      );
+      const data = await res.json();
+      setWeather(data);
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      setError(true);
+    }
+  }
+
   return (
-    weather && (
-      <div className="p-[0.5rem] absolute top-0 right-0 flex items-center justify-center  gap-2">
-        <p className="text-primary text-[1rem] text-center font-bold">
-          {weather.location.name}
-        </p>
-        <p className="text-primary text-[1rem] text-center font-bold">
-          {weather.current.temp_c}°C
-        </p>
-        <img
-          src={weather.current.condition.icon}
-          className="w-[3rem] h-[3rem]"
-        />
-      </div>
-    )
+    <div>
+
+    </div>
   );
 };
 
